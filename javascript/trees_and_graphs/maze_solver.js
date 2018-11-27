15f56f8d0f04f7147c52b45dd8d394aa1f5d416a
Josh <joshuaadamparker@hotmail.com>
2018-11-26 23:24:02 -0800

    add test for js solution of maze solver

--- javascript/trees_and_graphs/maze_solver.js
+++ javascript/trees_and_graphs/maze_solver.js
@@ -1,41 +1,45 @@
 /*
-xxx
+Given a 2D array of black and white entries representing a maze 
+with designated entrance and exit points, find [the shortest] path 
+from the entrance to the exit, if one exist.
 
-source: EPI xxx
+source: problem adapted from EPI xxx
 */
 
-function mazeSolver(maze, start, end) {
+function mazeSolver(maze, start, [end_row, end_col]) {
 
     let solution;
 
-    function _solve({row, col}, path){
+    function _solve([row, col], path){
 
         // this path is too long, abort!
         if ( solution && path.length >= solution.length ) {
             return;
         }
         // found best solution so far!
-        if (row === end.row && col === end.col) {
-            solution = path;
+        if (row === end_row && col === end_col) {
+            solution = [...path, [row,col]];
             return;
         } 
         
-        let prev = path[path.length -1]        
+        let [prev_row, prev_col] = (path.length > 0) 
+            ? path[path.length - 1] 
+            : [-1, -1];   
 
         // valid move respects boundries and doesn't backtrack
-        const isValidMove = ({row, col}) => (
+        const isValidMove = ([row, col]) => (
             row >= 0 && row < maze.length &&
             col >= 0 && col < maze[0].length &&
             maze[row][col] !== 1 &&
-            (row === prev.row && col === prev.col)
+            !(row === prev_row && col === prev_col)
         )
 
         let nexts = [
-            { row: row + 1, col },
-            { row: row - 1, col },
-            { row, col: col + 1 },
-            { row, col: col - 1 }
-        ].filter( next => isValidMove(next))
+            [ row + 1, col ],
+            [ row - 1, col ],
+            [ row, col + 1 ],
+            [ row, col - 1 ]
+        ].filter( next => isValidMove(next) )
         
         // try moving in every valid direction
         nexts.forEach(next => {
@@ -46,3 +50,20 @@ function mazeSolver(maze, start, end) {
     _solve(start, [])
     return solution;
 }
+
+// test
+
+describe('mazeSolver', function() {
+    beforeEach(function() {
+        this.maze = [
+            [0,0,1,0,0],
+            [0,1,1,0,1],
+            [0,1,0,0,0],
+            [0,0,0,1,0],
+            [0,1,0,1,0]
+        ]
+    })
+    it('find shortest solution to maze', function() {
+        expect(mazeSolver(this.maze, [0,0], [4,4])).toEqual([[0,0],[1,0],[2,0],[3,0],[3,1],[3,2],[2,2],[2,3],[2,4],[3,4],[4,4]])
+    })
+})
