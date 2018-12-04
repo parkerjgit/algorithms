dc70e3b31dd93737bc3f2c46c2926826ffd619b8
Josh <joshuaadamparker@hotmail.com>
2018-12-04 00:35:10 -0800

    edit js solution for build order

--- javascript/trees_and_graphs/build_order.js
+++ javascript/trees_and_graphs/build_order.js
@@ -11,6 +11,13 @@ function Project(name) {
     this.visited = false;
 }
 
+function printGraph(root) {
+    console.log(root.name);
+    root.dependancies.forEach(dep => {
+        printGraph(dep)
+    })
+}
+
 function buildGraph(prjs, deps) {
 
     let root = new Project('root');
@@ -22,34 +29,21 @@ function buildGraph(prjs, deps) {
         let {created: depNew, node: depNode} = findOrCreate(root, dep),
             {created: prjNew, node: prjNode} = findOrCreate(root, prj);
 
-        // console.log(`created ${prjNode.name} and ${depNode.name}`)
-
-        console.log(`dep ${dep} is new: ${depNew}`)
-
         // update graph
         if ( depNew ) {
-            // dependancy is a new proj
             seen.push(dep);
         } else {
-            // dependancy is not a new proj, so make sure not a child of root anymore
-            console.log(`before: ${JSON.stringify(root.dependancies.length)}`)
-            let idx = getDependancyIdx(root, prj) 
-            root.dependancies.splice(idx, 1)
-            console.log(`after: ${JSON.stringify(root.dependancies.length)}`)
+            let idx = getDependancyIdx(root, dep) 
+            if (idx >= 0) {
+                root.dependancies.splice(idx, 1)
+            }
         }
-
-        console.log(`prj ${prj} is new: ${prjNew}`)
-
         if ( prjNew ) {
-            // project is a new project
-            console.log(`add: ${JSON.stringify(root.dependancies.length)}`)
             seen.push(prj);
             root.dependancies.push(prjNode)
-            console.log(`add2: ${JSON.stringify(root.dependancies.length)}`)
         } 
-
         prjNode.dependancies.push(depNode);
-        
+
     })
 
     // add unseen projects to graph
@@ -57,6 +51,9 @@ function buildGraph(prjs, deps) {
         .forEach( prj => {
             root.dependancies.push(new Project(prj))
     })
+
+    console.log('---')
+    printGraph(root)
 
     return root;
 }
@@ -75,23 +72,21 @@ function findOrCreate(root, target){
 }
 
 function getProject(root, target) {
-    //console.log(`looking for ${target} on ${root.name}`)
+
     if (root.name === target)
         return root;
     if (root.dependancies.length === 0)
         return null;
 
-    //let visited = {};
+    let visited = new Set();
 
     return root.dependancies.reduce( (result, dep) => {
-        //if ( !visited[dep] ) {
-            //console.log(`recurse on ${dep.name}`)
-            //dep.visited = true;
-            // visited[dep] = true;
+        if (!visited.has(dep)) {
+            visited.add(dep);
             return result || getProject(dep, target);
-        //} else {
-            //return result;
-        //}
+        } else {
+            return result;
+        }
     }, null);
 }
 
@@ -106,11 +101,10 @@ function dftPostOrder(root) {
         })
         // then visit project
         root.visited = true;
-        console.log(`push ${root.name}`)
         order.push(root.name)
     }   
     _dft(root);
-    return order;
+    return order.slice(0, -1);
 }
 
 // function dftPostOrder(root) {
@@ -136,61 +130,56 @@ function buildOrder(prjs, deps) {
 
 // test
 
-// describe('getProject', function() {
-//     beforeEach(function() {
-//         this.root = new Project('root');
-//         this.a = new Project('a');
-//         this.b = new Project('b');
-//         this.c = new Project('c');
-//         this.d = new Project('d');
-//         this.e = new Project('e');
-//         this.f = new Project('f');
-//         this.root.dependancies.push(this.c);
-//         this.c.dependancies.push(this.d)
-//         this.d.dependancies.push(this.a)
-//         this.d.dependancies.push(this.b)
-//         this.a.dependancies.push(this.f)
-//         this.b.dependancies.push(this.f)
-//         this.b.dependancies.push(this.e)
-//     })
-//     it('gets a project node or null from a a-cyclic graph', function() {
-//         expect(getProject(this.root,'root').name).toEqual('root');
-//     })
-//     it('gets a project node or null from a a-cyclic graph', function() {
-//         expect(getProject(this.root,'c').name).toEqual('c');
-//     })
-//     it('gets a project node or null from a a-cyclic graph', function() {
-//         expect(getProject(this.root,'d').name).toEqual('d');
-//     })
-//     it('gets a project node or null from a a-cyclic graph', function() {
-//         expect(getProject(this.root,'a').name).toEqual('a');
-//     })
-//     it('gets a project node or null from a a-cyclic graph', function() {
-//         expect(getProject(this.root,'x')).toEqual(null);
-//     })
-// })
-describe('buildGraph', function() {
-    // it('builds a graph from edges and nodes', function() {
-    //     expect(
-    //         buildGraph(['a','b','c','d','e','f'],[['a','d'],['f','b'],['b','d'],['f','a'],['d','c'],['e','b'],['b','a']]).name
-    //     ).toEqual(
-    //         'root'
-    //     );
-    // })
-    it('builds a graph from edges and nodes', function() {
-        expect(
-            buildGraph(['a','b','c','d','e','f'],[['a','d'],['f','b'],['b','d'],['f','a'],['d','c'],['e','b'],['b','a']]).dependancies.length
-        ).toEqual(
-            1
-        );
+describe('getProject', function() {
+    beforeEach(function() {
+        this.root = new Project('root');
+        this.a = new Project('a');
+        this.b = new Project('b');
+        this.c = new Project('c');
+        this.d = new Project('d');
+        this.e = new Project('e');
+        this.f = new Project('f');
+        this.root.dependancies.push(this.c);
+        this.c.dependancies.push(this.d)
+        this.d.dependancies.push(this.a)
+        this.d.dependancies.push(this.b)
+        this.a.dependancies.push(this.f)
+        this.b.dependancies.push(this.f)
+        this.b.dependancies.push(this.e)
+    })
+    it('gets a project node or null from a a-cyclic graph', function() {
+        expect(getProject(this.root,'root').name).toEqual('root');
+    })
+    it('gets a project node or null from a a-cyclic graph', function() {
+        expect(getProject(this.root,'c').name).toEqual('c');
+    })
+    it('gets a project node or null from a a-cyclic graph', function() {
+        expect(getProject(this.root,'d').name).toEqual('d');
+    })
+    it('gets a project node or null from a a-cyclic graph', function() {
+        expect(getProject(this.root,'a').name).toEqual('a');
+    })
+    it('gets a project node or null from a a-cyclic graph', function() {
+        expect(getProject(this.root,'x')).toEqual(null);
     })
 })
-// describe('buildOrder', function() {
-//     it('gets topological order of set of projects and dependancies', function() {
+
+// describe('buildGraph', function() {
+//     it('builds a graph from edges and nodes', function() {
 //         expect(
-//             buildOrder(['a','b','c','d','e','f'],[['a','d'],['f','b'],['b','d'],['f','a'],['d','e'],['e','b'],['b','a']])
+//             buildGraph(['a','b','c','d','e','f'],[['a','d'],['f','b'],['b','d'],['f','a'],['d','c'],['e','b'],['b','a']]).dependancies.length
 //         ).toEqual(
-//             ['f','e','b','a','d','e']
+//             1
 //         );
 //     })
 // })
+
+describe('buildOrder', function() {
+    it('gets topological order of set of projects and dependancies', function() {
+        expect(
+            buildOrder(['a','b','c','d','e','f'],[['a','d'],['f','b'],['b','d'],['f','a'],['d','c'],['e','b'],['b','a']])
+        ).toEqual(
+            ['f','e','b','a','d','c']
+        );
+    })
+})
