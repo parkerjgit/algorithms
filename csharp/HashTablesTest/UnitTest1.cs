68cefde8707eb4a64ca2fd0af38e52047f8f4020
Joshua Parker <joshuaparker@protonmail.com>
2021-05-08 09:55:50 -0700

    add cs solution to nsum problem

---
+++ csharp/HashTablesTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HashTables;
+
+namespace HashTablesTest
+{
+    [TestClass]
+    public class TwoSumTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            int[] result = NSum.TwoSum(new int[] { 2, 4, 5, 6, 7, 8, 9 }, 16);
+
+            Assert.AreEqual(4, result[0]);
+            Assert.AreEqual(6, result[1]);
+        }
+    }
+
+    [TestClass]
+    public class ThreeSumTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            int[] result = NSum.ThreeSum(new int[] { 2, 4, 5, 6, 7, 8, 9 }, 24);
+
+            Assert.AreEqual(4, result[0]);
+            Assert.AreEqual(5, result[1]);
+            Assert.AreEqual(6, result[2]);
+        }
+    }
+}
---
+++ csharp/UnionFind/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnionFind
+{
+    public class Class1
+    {
+    }
+}
